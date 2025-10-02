8489969da85012b4625fb259a8544ce6196b372f
iakov2393 <iakov2393@gmail.com>
2025-10-02 19:46:59 +0300

    |

--- src/events/views.py
+++ src/events/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from models import Event, Place
+from .models import Event, Place
 from django.utils import timezone
 
 
